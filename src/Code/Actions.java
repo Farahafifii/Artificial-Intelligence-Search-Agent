b5df4124e95e7646a3ab2d92ff68490d2db36d57
Mennaabdallah7 <127664026+mennaabdullah@users.noreply.github.com>
2023-11-08 01:32:32 +0200

    material/energy/food

--- src/Code/Actions.java
+++ src/Code/Actions.java
@@ -5,13 +5,16 @@ import javax.swing.*;
 public class Actions extends GenericSearch {
 
 public void RequestFood() {
-
+    this.initialFood--;
+    this.monetary_cost= this.unitPriceFood+this.monetary_cost;
 }
 public void RequestMaterials() {
-	
+	this.initialMaterials--;
+    this.monetary_cost= this.unitPriceMaterials+this.monetary_cost;
 }
 public void RequestEnergy() {
-	
+    this.initialEnergy--;
+	this.monetary_cost= this.unitPriceEnergy+this.monetary_cost;
 }
 public void Build1() {
 	
