0fc1d85bd546af5ba60e4d48985957aac08d2421
Mennaabdallah7 <127664026+mennaabdullah@users.noreply.github.com>
2023-11-08 01:43:11 +0200

    material/energy/food/build1/build2

--- src/Code/Actions.java
+++ src/Code/Actions.java
@@ -10,19 +10,42 @@ public void RequestFood() {
 }
 public void RequestMaterials() {
 	this.initialMaterials--;
-    this.monetary_cost= this.unitPriceMaterials+this.monetary_cost;
+    this.monetary_cost += this.unitPriceMaterials;
 }
 public void RequestEnergy() {
     this.initialEnergy--;
-	this.monetary_cost= this.unitPriceEnergy+this.monetary_cost;
+	this.monetary_cost += this.unitPriceEnergy;
 }
 public void Build1() {
-	
+    this.monetary_cost += this.priceBUILD1;
+    this.prosperity+=prosperityBUILD1;
+
+    for(int i=0; i<this.materialsUseBUILD1;i++){
+        RequestMaterials();
+    }
+    for(int i=0; i<this.energyUseBUILD1;i++){
+        RequestEnergy();
+    }
+    for(int i=0; i<this.foodUseBUILD1;i++){
+        RequestFood();
+    }
 }
+    public void Build2() {
+        this.monetary_cost += this.priceBUILD2;
+        this.prosperity+=prosperityBUILD2;
+
+        for(int i=0; i<this.materialsUseBUILD2;i++){
+            RequestMaterials();
+        }
+        for(int i=0; i<this.energyUseBUILD2;i++){
+            RequestEnergy();
+        }
+        for(int i=0; i<this.foodUseBUILD2;i++){
+            RequestFood();
+        }
+    }
 public void Wait() {
 	
 }
-public void Build2() {
-	
-}
+
 }
